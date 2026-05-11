c09dd650709b17ec4dab05781073847aa98d98cd
Papsanly <papsanlysenko@gmail.com>
2026-05-11 16:36:09 +0300

    do some reformating

--- main/led_strip_encoder.c
+++ main/led_strip_encoder.c
@@ -19,12 +19,10 @@ typedef struct {
 } rmt_led_strip_encoder_t;
 
 RMT_ENCODER_FUNC_ATTR
-static size_t rmt_encode_led_strip(rmt_encoder_t *encoder,
-                                   rmt_channel_handle_t channel,
+static size_t rmt_encode_led_strip(rmt_encoder_t *encoder, rmt_channel_handle_t channel,
                                    const void *primary_data, size_t data_size,
                                    rmt_encode_state_t *ret_state) {
-  rmt_led_strip_encoder_t *led_encoder =
-      __containerof(encoder, rmt_led_strip_encoder_t, base);
+  rmt_led_strip_encoder_t *led_encoder = __containerof(encoder, rmt_led_strip_encoder_t, base);
   rmt_encoder_handle_t bytes_encoder = led_encoder->bytes_encoder;
   rmt_encoder_handle_t copy_encoder = led_encoder->copy_encoder;
   rmt_encode_state_t session_state = RMT_ENCODING_RESET;
@@ -32,11 +30,10 @@ static size_t rmt_encode_led_strip(rmt_encoder_t *encoder,
   size_t encoded_symbols = 0;
   switch (led_encoder->state) {
   case 0: // send RGB data
-    encoded_symbols += bytes_encoder->encode(
-        bytes_encoder, channel, primary_data, data_size, &session_state);
+    encoded_symbols +=
+        bytes_encoder->encode(bytes_encoder, channel, primary_data, data_size, &session_state);
     if (session_state & RMT_ENCODING_COMPLETE) {
-      led_encoder->state =
-          1; // switch to next state when current encoding session finished
+      led_encoder->state = 1; // switch to next state when current encoding session finished
     }
     if (session_state & RMT_ENCODING_MEM_FULL) {
       state |= RMT_ENCODING_MEM_FULL;
@@ -44,12 +41,10 @@ static size_t rmt_encode_led_strip(rmt_encoder_t *encoder,
     }
   // fall-through
   case 1: // send reset code
-    encoded_symbols +=
-        copy_encoder->encode(copy_encoder, channel, &led_encoder->reset_code,
-                             sizeof(led_encoder->reset_code), &session_state);
+    encoded_symbols += copy_encoder->encode(copy_encoder, channel, &led_encoder->reset_code,
+                                            sizeof(led_encoder->reset_code), &session_state);
     if (session_state & RMT_ENCODING_COMPLETE) {
-      led_encoder->state =
-          RMT_ENCODING_RESET; // back to the initial encoding session
+      led_encoder->state = RMT_ENCODING_RESET; // back to the initial encoding session
       state |= RMT_ENCODING_COMPLETE;
     }
     if (session_state & RMT_ENCODING_MEM_FULL) {
@@ -63,8 +58,7 @@ out:
 }
 
 static esp_err_t rmt_del_led_strip_encoder(rmt_encoder_t *encoder) {
-  rmt_led_strip_encoder_t *led_encoder =
-      __containerof(encoder, rmt_led_strip_encoder_t, base);
+  rmt_led_strip_encoder_t *led_encoder = __containerof(encoder, rmt_led_strip_encoder_t, base);
   rmt_del_encoder(led_encoder->bytes_encoder);
   rmt_del_encoder(led_encoder->copy_encoder);
   free(led_encoder);
@@ -73,8 +67,7 @@ static esp_err_t rmt_del_led_strip_encoder(rmt_encoder_t *encoder) {
 
 RMT_ENCODER_FUNC_ATTR
 static esp_err_t rmt_led_strip_encoder_reset(rmt_encoder_t *encoder) {
-  rmt_led_strip_encoder_t *led_encoder =
-      __containerof(encoder, rmt_led_strip_encoder_t, base);
+  rmt_led_strip_encoder_t *led_encoder = __containerof(encoder, rmt_led_strip_encoder_t, base);
   rmt_encoder_reset(led_encoder->bytes_encoder);
   rmt_encoder_reset(led_encoder->copy_encoder);
   led_encoder->state = RMT_ENCODING_RESET;
@@ -85,11 +78,9 @@ esp_err_t rmt_new_led_strip_encoder(const led_strip_encoder_config_t *config,
                                     rmt_encoder_handle_t *ret_encoder) {
   esp_err_t ret = ESP_OK;
   rmt_led_strip_encoder_t *led_encoder = NULL;
-  ESP_GOTO_ON_FALSE(config && ret_encoder, ESP_ERR_INVALID_ARG, err, TAG,
-                    "invalid argument");
+  ESP_GOTO_ON_FALSE(config && ret_encoder, ESP_ERR_INVALID_ARG, err, TAG, "invalid argument");
   led_encoder = rmt_alloc_encoder_mem(sizeof(rmt_led_strip_encoder_t));
-  ESP_GOTO_ON_FALSE(led_encoder, ESP_ERR_NO_MEM, err, TAG,
-                    "no mem for led strip encoder");
+  ESP_GOTO_ON_FALSE(led_encoder, ESP_ERR_NO_MEM, err, TAG, "no mem for led strip encoder");
   led_encoder->base.encode = rmt_encode_led_strip;
   led_encoder->base.del = rmt_del_led_strip_encoder;
   led_encoder->base.reset = rmt_led_strip_encoder_reset;
@@ -112,16 +103,14 @@ esp_err_t rmt_new_led_strip_encoder(const led_strip_encoder_config_t *config,
           },
       .flags.msb_first = 1 // WS2812 transfer bit order: G7...G0R7...R0B7...B0
   };
-  ESP_GOTO_ON_ERROR(
-      rmt_new_bytes_encoder(&bytes_encoder_config, &led_encoder->bytes_encoder),
-      err, TAG, "create bytes encoder failed");
+  ESP_GOTO_ON_ERROR(rmt_new_bytes_encoder(&bytes_encoder_config, &led_encoder->bytes_encoder), err,
+                    TAG, "create bytes encoder failed");
   rmt_copy_encoder_config_t copy_encoder_config = {};
-  ESP_GOTO_ON_ERROR(
-      rmt_new_copy_encoder(&copy_encoder_config, &led_encoder->copy_encoder),
-      err, TAG, "create copy encoder failed");
+  ESP_GOTO_ON_ERROR(rmt_new_copy_encoder(&copy_encoder_config, &led_encoder->copy_encoder), err,
+                    TAG, "create copy encoder failed");
 
-  uint32_t reset_ticks = config->resolution / 1000000 * 50 /
-                         2; // reset code duration defaults to 50us
+  uint32_t reset_ticks =
+      config->resolution / 1000000 * 50 / 2; // reset code duration defaults to 50us
   led_encoder->reset_code = (rmt_symbol_word_t){
       .level0 = 0,
       .duration0 = reset_ticks,
